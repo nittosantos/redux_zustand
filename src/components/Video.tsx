3f9b8ac9c9cd938827c9dec27f6ad97b4b647633
nittosantos <nittosantos.dev@gmail.com>
2024-01-18 16:45:14 -0300

    feat: :sparkles: aplicando introdução aos loadings e async thunk

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,10 +1,12 @@
 import ReactPlayer from "react-player";
-import { useDispatch } from "react-redux";
 import { next, useCurrentLesson } from "../store/slices/player";
+import { useAppDispatch, useAppSelector } from "../store";
+import { Loader2 } from "lucide-react";
 
 export function Video() {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const { currentLesson } = useCurrentLesson();
+  const isCourseLoading = useAppSelector((state) => state.player.isLoading);
 
   const handlePlayNext = () => {
     dispatch(next());
@@ -12,14 +14,20 @@ export function Video() {
 
   return (
     <div className="w-full bg-zinc-950 aspect-video">
-      <ReactPlayer
-        width="100%"
-        height="100%"
-        controls
-        playing
-        onEnded={handlePlayNext}
-        url={`https://www.youtube.com/watch?v=${currentLesson?.id}`}
-      />
+      {isCourseLoading ? (
+        <div className="flex h-full items-center justify-center">
+          <Loader2 className="w-8 h-8 text-emerald-400 animate-spin" />
+        </div>
+      ) : (
+        <ReactPlayer
+          width="100%"
+          height="100%"
+          controls
+          playing
+          onEnded={handlePlayNext}
+          url={`https://www.youtube.com/watch?v=${currentLesson?.id}`}
+        />
+      )}
     </div>
   );
 }
--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,5 +1,6 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { useAppSelector } from "..";
+import { api } from "../../lib/axios";
 
 interface Course {
   id: number;
@@ -18,22 +19,25 @@ export interface PlayerState {
   course: Course | null;
   currentModuleIndex: number;
   currentLessonIndex: number;
+  isLoading: boolean;
 }
 
 const initialState: PlayerState = {
   course: null,
   currentModuleIndex: 0,
   currentLessonIndex: 0,
+  isLoading: true,
 };
+
+export const loadCourse = createAsyncThunk("player/load", async () => {
+  const response = await api.get("/courses/1");
+  return response.data;
+});
 
 const playerSlice = createSlice({
   name: "player",
   initialState,
   reducers: {
-    start: (state, action: PayloadAction<Course>) => {
-      state.course = action.payload;
-    },
-
     play: (state, action: PayloadAction<[number, number]>) => {
       state.currentModuleIndex = action.payload[0];
       state.currentLessonIndex = action.payload[1];
@@ -58,10 +62,19 @@ const playerSlice = createSlice({
       }
     },
   },
+  extraReducers(builder) {
+    builder.addCase(loadCourse.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(loadCourse.fulfilled, (state, action) => {
+      state.course = action.payload;
+      state.isLoading = false;
+    });
+  },
 });
 
 export const player = playerSlice.reducer;
-export const { play, next, start } = playerSlice.actions;
+export const { play, next } = playerSlice.actions;
 
 export const useCurrentLesson = () => {
   return useAppSelector((state) => {
